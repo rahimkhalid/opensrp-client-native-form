6c469f97aa8b677fde3a99c667695135595f8a7d
waqas_j35 <waqas@venturedive.com>
2020-10-20 11:25:57 +0500

    fixed average calculation when inputs are decimal values

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/activities/JsonFormActivity.java
+++ android-json-form-wizard/src/main/java/com/vijay/jsonwizard/activities/JsonFormActivity.java
@@ -1837,7 +1837,7 @@ public class JsonFormActivity extends JsonFormBaseActivity implements JsonApi {
         Object jsonObject = object;
         try {
             if (jsonObject.toString().contains(".")) {
-                jsonObject = String.valueOf((float) Math.round(Float.valueOf(jsonObject.toString()) * 100) / 100);
+                jsonObject = ((double) Math.round(Double.parseDouble(jsonObject.toString()) * 100) / 100);
             } else {
                 jsonObject = Integer.valueOf(jsonObject.toString());
             }
@@ -1856,14 +1856,14 @@ public class JsonFormActivity extends JsonFormBaseActivity implements JsonApi {
         if (object != null) {
             String objectKey = addressString.replace(":", "_");
             try {
-//                if(!object.getString(JsonFormConstants.TYPE).equals(JsonFormConstants.SPINNER)) {
+                if(!object.getString(JsonFormConstants.TYPE).equals(JsonFormConstants.SPINNER)) {
                     formValuesCacheMap.remove(objectKey);
                     formValuesCacheMap.put(objectKey, "");
                     if (object.has(JsonFormConstants.VALUE)) {
                         object.remove(JsonFormConstants.VALUE);
                     }
-//                }
-            } catch (JSONException e) {
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
